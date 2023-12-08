4ef8ad1bfa6dc7248c4d177a3d8ce9803b7d3fa0
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2023-12-07 22:28:03 -0600

    Complete Knight Travails

--- knights.rb
+++ knights.rb
@@ -1,10 +1,9 @@
 
 
 class Destination
-  attr_accessor :location, :predecessor
-  def initialize(coords, predecessor = nil)
+  attr_accessor :location
+  def initialize(coords)
     @location = coords
-    @predecessor = predecessor
   end
 
   def potential_moves(coords = self.location)
@@ -12,9 +11,10 @@ class Destination
     destinations = move_vectors.map { |x, y| [coords[0] + x, coords[1] + y] }
     destinations.select { |x, y| x.between?(0, 7) && y.between?(0, 7) }
   end
+end
 
 class Knight
-  attr_accessor :visited, :starting_square
+  attr_accessor :visited, :start
 
 
   def initialize(starting_square = [0,0])
@@ -23,12 +23,31 @@ class Knight
   end
 
   def knight_moves(starting_square = self.start, target_square)
-    queue = [self.start]
+    queue = [starting_square]
 
-    until queue.empty? or self.visited.include?(target_square)
+    until queue.empty? || self.visited.include?(target_square)
       square_to_explore = queue.shift
-      while target_square
+      potential_moves = square_to_explore.potential_moves
+      deduped_moves = potential_moves.select {|destination| !self.visited.include?(destination)}
+      # !self.visited.inclue?(destination.location)}
+      deduped_moves.each do |destination|
+        self.visited[destination] = square_to_explore.location
+        queue << Destination.new(destination)
+      end
+
     end
+    # reversing the path
+    reverse_square = target_square
+    path = [reverse_square]
+    until reverse_square == starting_square.location
+      reverse_square = self.visited[reverse_square]
+      path <<reverse_square
+    end
+    # path << starting_square.location
+    path = path.reverse
+
+    puts "You made it in #{path.length} moves! Here's your path"
+    path.each {|x,y| puts "[#{x},#{y}]"}
 
   end
 
@@ -36,3 +55,6 @@ end
 
 
 board = Destination.new([2,1])
+knight = Knight.new([3,3])
+knight.knight_moves([0,0])
+# p knight.visited
